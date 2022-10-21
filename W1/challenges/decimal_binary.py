675119dde069e13d050427b2bae2bd5266772f5c
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2022-10-21 15:33:19 +0500

    Add challenges for W1

---
+++ W1/challenges/decimal_binary.py
@@ -0,0 +1,6 @@
+'''
+Write a program to:
+
+- Convert an binary number into a decimal number.
+- Convert an decimal number into a binary number.
+'''
---
+++ W1/challenges/fizz_buzz.py
@@ -0,0 +1,9 @@
+'''
+Write a program that takes two numbers as input from the user, e.g., n1 and n2.
+
+For a sequence of numbers from 1 to 32:
+    - Print "Fizz" for multiples of n1.
+    - Print "Buzz" for multiples of n2.
+    - Print "FizzBuzz" for common multiples of n1 and n2 .
+    - Otherwise, print the number itself.
+'''
